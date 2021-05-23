d603dcda8044d60c22ad9684e281704ac745877f
Akhil Kondabathini <kondabathiniakhil@gmail.com>
2021-05-23 15:11:13 +0530

    changed anchor tag to div for pagination fixing the error 404 on website

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Pagination = ({postsPerPage, totalPosts, paginate}) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
 
-    const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    for(let i = 1; i <= Math.ceil(totalPosts/postsPerPage); i++){
-        pageNumbers.push(i);
-    }
+  return (
+    <nav className="pagination">
+      {pageNumbers.map((pageNumber) => (
+        <li key={pageNumber} className="page-item">
+          <div
+            style={{ cursor: "pointer" }}
+            className="page-link"
+            onClick={() => paginate(pageNumber)}
+          >
+            {pageNumber}
+          </div>
+        </li>
+      ))}
+    </nav>
+  );
+};
 
-    return (
-        <nav className="pagination">
-            {pageNumbers.map(pageNumber =>(
-                <li key={pageNumber} className="page-item">
-                    <a href="!#"
-                className="page-link"
-                onClick={() => paginate(pageNumber)}>
-                    {pageNumber}
-                </a>
-                </li>
-                
-            ))}
-        </nav>
-    )
-}
-
-export default Pagination
+export default Pagination;
